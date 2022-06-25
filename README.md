0e1764686b0d764bf663d812e216de8196ee92e0
Tyrone Ward <gameplace123@gmail.com>
2022-06-24 22:51:01 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# node-js-template
+
+A barebones Node.js app using [Express 4](http://expressjs.com/).
+
+## Running Locally
+
+Make sure you have [Node.js](http://nodejs.org/) and the [nodemon](https://www.npmjs.com/package/nodemon) installed.
+
+```sh
+$ git clone git@github.com:jomaoppa/node-js-template.git # or clone your own fork
+$ cd node-js-template
+$ npm install
+$ npm start
+```
+
+Your app should now be running on [localhost:5000](http://localhost:5000/).
+
+## Deploying to Heroku
+
+```
+$ heroku create
+$ git push heroku master
+$ heroku open
+```
+
+or
+
+[![Deploy to Heroku](https://www.herokucdn.com/deploy/button.png)](https://heroku.com/deploy)
---
+++ app.js
@@ -0,0 +1,25 @@
+#! /usr/bin/env node
+
+require('dotenv').config()
+
+const express = require('express')
+const logger = require('morgan')
+
+const app = express()
+
+const PORT = process.env.PORT || 3000
+
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+app.use(logger('common')) // Standard Apache common log output.
+app.use(express.static('public'))
+// app.use('/', require('./routes/index'))
+
+const localIp = Object.values(require('os').networkInterfaces())
+  .flat()
+  .filter((item) => !item.internal && item.family === 'IPv4')
+  .find(Boolean).address
+
+app.listen(PORT, () => {
+  console.log(`Service listening at http://${localIp}:${PORT}`)
+})
